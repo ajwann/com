b430be2355720048fd52907b25cd7308f73a0c54
Adam Wanninger <ajwann@ajwann.com>
2019-12-21 10:06:01 -0700

    flexbox height and width is now same as particles js div

--- docs/script.js
+++ docs/script.js
@@ -1,5 +1,5 @@
 var message = `
-Hi there, my name is Adam. I'm a software engineer with expertise in Ruby
+Hi, my name is Adam. I'm a software engineer with expertise in Ruby
     on Rails. I also have experience with front end Javascript frameworks 
     such as Ember and React. I'm passionate about open source software, and in my free time I like
     to contribute to <a class="link-text" href='https://github.com/bundler/bundler/commits?author=ajwann'>Bundler</a>
@@ -8,18 +8,30 @@ Hi there, my name is Adam. I'm a software engineer with expertise in Ruby
   local Ruby on Rails scene. You can often find me at the 
   <a class="link-text" href='https://www.meetup.com/Mutually-Human-Hack-Night'>Mutually Human Hack Night</a>
   and the <a class="link-text" href='http://columbusrb.com/'>Columbus Ruby Brigade</a>.
+<br />
+<br />
+<a class="link-text" href="mailto:ajwann@ajwann.com">Get in touch</a>
 `;
 
-var about = document.getElementById("about")
+var github = document.getElementById("github");
+var blog = document.getElementById("blog");
+var flexParent = github.parentNode;
+var about = document.getElementById("about");
 var infoIconHTML = about.innerHTML;
 
 about.addEventListener('click', function(e) {
+  // temporarily remove other links from the DOM
+  flexParent.removeChild(github);
+  flexParent.removeChild(blog);
+  // replace the info icon with actual info
   about.innerHTML = 
     '<div class="about-content"><div id="about-content-exit"><i class="fas fa-window-close"></i></div>' + message + '</div>';
   
   var aboutExit = document.getElementById("about-content-exit");
   aboutExit.addEventListener('click', function(e) {
   e.stopPropagation();
+  flexParent.appendChild(github);
+  flexParent.appendChild(blog);
   about.innerHTML = infoIconHTML;
 })
 });
