55b23b2434155c019e84eb9adb25033906e72f38
ajwann <ajwann@ajwann.com>
2021-05-22 14:57:43 -0400

    the animation works when am alert is present

--- docs/script.js
+++ docs/script.js
@@ -21,6 +21,7 @@ var infoIconHTML = about.innerHTML;
 
 document.addEventListener("DOMContentLoaded", function(event) { 
   setTimeout(function() {
+    alert('Hello!');
     about.classList.add("simulate-hover");
     github.classList.add("simulate-hover");
     resume.classList.add("simulate-hover");
